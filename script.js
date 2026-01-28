c7752dfa8127641834df7a161d970e12bb75f5b0
ielclem <ielsethclemente@gmail.com>
2026-01-28 19:16:49 +0000

    Exercise 2

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 600);
 }
 function draw() {
 //   background(0);
@@ -19,10 +19,16 @@ function draw() {
   textSize(30);
   text("Canada's Exports", 200, 40);
 
-  Object.values(list).map((item, index) => {   
-    rect(50, index * 30 + 70, item.amount, 10);
+  Object.values(list).map((item, index) => {  
+    fill(item.color);
+    rect(50, index * 30 + 70, item.amount / 180 * 300, 10);
+    fill("black");
     textSize(10);
     textAlign("left");
     text(item.name, 50, index * 30 + 65);
+
+    textSize(20);
+    textAlign("right");
+    text(item.icon, 45, index * 30 + 75);
   })
 }
